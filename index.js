1582080431a2ca0e8f1c734ecbebf7dfba5faa59
Andrew McLees <andrewmclees5@gmail.com>
2018-04-03 22:10:34 -0700

    Finish initial websocket server

--- index.js
+++ index.js
@@ -3,14 +3,32 @@ let app = express();
 let http = require('http').Server(app);
 let io = require('socket.io')(http);
 
-let socket = require('./lib/socket');
+const rooms = {};
+io.on('connection', function(socket) {
+  socket.emit('you', socket.id);
+  socket.emit('rooms', rooms);
 
-io.on('connection', socket.connectionHandler({
-  'io': io
-}));
-app.use(express.static('public'));
+  socket.on('room', function(room) {
+    if (rooms[room]) {
+      socket.emit('room exists');
+    } else {
+      socket.join(room);
+      rooms[room] = true;
+      socket.emit('room created');
+    }
+  });
+
+  socket.on('join', function(room) {
+    socket.join(room);
+    socket.broadcast.to(room).emit('joined', socket.id);
+  });
+
+  socket.on('send', function(room, buffer) {
+    socket.broadcast.to(room).emit('broadcast', buffer);
+  });
+});
 
 const port = 3000;
 http.listen(port, function() {
-  console.log(`Started server on port ${port}`);
+  console.log(`Started websocket server on port ${port}`);
 });
