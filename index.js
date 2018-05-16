5c109ad1ffb371fcd32f20b7362b465f3dd7f35a
amclees <andrewmclees5@gmail.com>
2018-05-16 15:28:33 -0700

    Track members in each room

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ let http = require('http').Server(app);
 let io = require('socket.io')(http);
 
 const rooms = {
-  'lobby': true
+  'lobby': []
 };
 io.on('connection', function(socket) {
   socket.join('lobby');
@@ -16,21 +16,39 @@ io.on('connection', function(socket) {
     if (rooms[room]) {
       socket.emit('room exists');
     } else {
+      socket.leave('lobby');
       socket.join(room);
-      rooms[room] = true;
+      rooms[room] = [socket.id];
       socket.emit('room created');
       socket.broadcast.to('lobby').emit('new room', room);
     }
   });
 
   socket.on('join', function(room) {
+    if (!rooms[room]) {
+      socket.emit('room does not exist');
+      return;
+    }
     socket.leave('lobby');
+    rooms[room].push(socket.id);
     socket.join(room);
+    socket.emit('others', rooms[room]);
     socket.broadcast.to(room).emit('joined', socket.id);
   });
 
-  socket.on('send', function(room, buffer) {
-    socket.broadcast.to(room).emit('broadcast', buffer);
+  socket.on('send', function(room, tag, buffer) {
+    socket.broadcast.to(room).emit('broadcast', tag, buffer);
+  });
+
+  socket.on('disconnect', function() {
+    for (let room of Object.keys(rooms)) {
+      let roomMembers = rooms[room];
+      for(let i = roomMembers.length - 1; i >= 0; i--) {
+        if(roomMembers[i] === socket.id) {
+           roomMembers.splice(i, 1);
+        }
+      }
+    }
   });
 });
 
